4ee4732d1cff31d14972ccf2227d9ece0b15df0f
PROagario <procoderzphisftw@gmail.com>
2015-06-13 16:17:53 +0100

    Changed nick
    I changed the nick to make it look more legit

--- launcher.user.js
+++ launcher.user.js
@@ -884,7 +884,7 @@ console.log("Running Bot Launcher!");
   dArc = [],
   dText = [],
   lines = [],
-  originalName = "NotReallyABot",
+  originalName = "Jamie",
   sessionScore = 0,
   serverIP = "",
   interNodes = [],
